fb5203f79154df9e5ab5769a69acdf9ad3e0c9d9
Bayu Kresna Mukti <161997521+Byblee38@users.noreply.github.com>
2024-05-23 09:29:40 +0700

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main() {
     for (int i = 0; i < 10; i++)
     {
-    cout<<"hello world";
-        /* code */
+        cout<<"hello world"<<endl;
     }
     
     return 0;
